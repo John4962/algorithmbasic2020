b92ccb872ae2852984d8dcec3f58b8ae98d6814f
王明星 <wangmingxing@126.com>
2023-01-10 14:50:21 +0800

    添加注释

--- src/class01/Code02_BubbleSort.java
+++ src/class01/Code02_BubbleSort.java
@@ -4,107 +4,121 @@ import java.util.Arrays;
 
 public class Code02_BubbleSort {
 
-	public static void bubbleSort(int[] arr) {
-		if (arr == null || arr.length < 2) {
-			return;
-		}
-		// 0 ~ N-1
-		// 0 ~ N-2
-		// 0 ~ N-3
-		for (int e = arr.length - 1; e > 0; e--) { // 0 ~ e
-			for (int i = 0; i < e; i++) {
-				if (arr[i] > arr[i + 1]) {
-					swap(arr, i, i + 1);
-				}
-			}
-		}
-	}
+    public static void bubbleSort(int[] arr) {
+        if (arr == null || arr.length < 2) return;
+        // 0 ~ N-1
+        // 0 ~ N-2
+        // 0 ~ N-3
+        // 注意观察, 变量在右侧. 从n-1开始, 到0为止.
+        for (int e = arr.length - 1; e >= 0; e--) {
+            boolean flag = false;
+            // [0,e] 在这个范围上
+            // 0,1     1,2    2,3    3,4   e-1,e
+            // i ,i+1
+            // 设i是第一个数, 则i+1是第二个数. 那么i 从0开始. 到e-1 为止 [0,e-1]
+            for (int i = 0; i < e; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    swap(arr, i, i + 1);
+                    flag = true;// 表示有数据交换.
+                }
+            }
+            // 还有另一种写法 ,设i 是最后一个数. 那么它前面一个数是i-1
+            // 0,1  1,2  2,3  e-1,e
+            // i-1,i
+            for (int i = 1; i <= e; i++) {
+                if (arr[i - 1] > arr[i]) {
+                    swap(arr, i - 1, i);
+                }
+            }
+            if (!flag) break;//没有数据交换, 说明已完成排序.跳出循环.
+        }
+    }
 
-	// 交换arr的i和j位置上的值
-	public static void swap(int[] arr, int i, int j) {
-		arr[i] = arr[i] ^ arr[j];
-		arr[j] = arr[i] ^ arr[j];
-		arr[i] = arr[i] ^ arr[j];
-	}
+    // 交换arr的i和j位置上的值
+    public static void swap(int[] arr, int i, int j) {
+        arr[i] = arr[i] ^ arr[j];
+        arr[j] = arr[i] ^ arr[j];
+        arr[i] = arr[i] ^ arr[j];
+    }
 
-	// for test
-	public static void comparator(int[] arr) {
-		Arrays.sort(arr);
-	}
+    // for test
+    public static void comparator(int[] arr) {
+        Arrays.sort(arr);
+    }
 
-	// for test
-	public static int[] generateRandomArray(int maxSize, int maxValue) {
-		int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
-		}
-		return arr;
-	}
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) ((maxValue + 1) * Math.random()) - (int) (maxValue * Math.random());
+        }
+        return arr;
+    }
 
-	// for test
-	public static int[] copyArray(int[] arr) {
-		if (arr == null) {
-			return null;
-		}
-		int[] res = new int[arr.length];
-		for (int i = 0; i < arr.length; i++) {
-			res[i] = arr[i];
-		}
-		return res;
-	}
+    // for test
+    public static int[] copyArray(int[] arr) {
+        if (arr == null) {
+            return null;
+        }
+        int[] res = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            res[i] = arr[i];
+        }
+        return res;
+    }
 
-	// for test
-	public static boolean isEqual(int[] arr1, int[] arr2) {
-		if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
-			return false;
-		}
-		if (arr1 == null && arr2 == null) {
-			return true;
-		}
-		if (arr1.length != arr2.length) {
-			return false;
-		}
-		for (int i = 0; i < arr1.length; i++) {
-			if (arr1[i] != arr2[i]) {
-				return false;
-			}
-		}
-		return true;
-	}
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	// for test
-	public static void printArray(int[] arr) {
-		if (arr == null) {
-			return;
-		}
-		for (int i = 0; i < arr.length; i++) {
-			System.out.print(arr[i] + " ");
-		}
-		System.out.println();
-	}
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr == null) {
+            return;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 
-	// for test
-	public static void main(String[] args) {		
-		int testTime = 500000;
-		int maxSize = 100;
-		int maxValue = 100;
-		boolean succeed = true;
-		for (int i = 0; i < testTime; i++) {
-			int[] arr1 = generateRandomArray(maxSize, maxValue);
-			int[] arr2 = copyArray(arr1);
-			bubbleSort(arr1);
-			comparator(arr2);
-			if (!isEqual(arr1, arr2)) {
-				succeed = false;
-				break;
-			}
-		}
-		System.out.println(succeed ? "Nice!" : "Fucking fucked!");
+    // for test
+    public static void main(String[] args) {
+        int testTime = 500000;
+        int maxSize = 100;
+        int maxValue = 100;
+        boolean succeed = true;
+        for (int i = 0; i < testTime; i++) {
+            int[] arr1 = generateRandomArray(maxSize, maxValue);
+            int[] arr2 = copyArray(arr1);
+            bubbleSort(arr1);
+            comparator(arr2);
+            if (!isEqual(arr1, arr2)) {
+                succeed = false;
+                break;
+            }
+        }
+        System.out.println(succeed ? "Nice!" : "Fucking fucked!");
 
-		int[] arr = generateRandomArray(maxSize, maxValue);
-		printArray(arr);
-		bubbleSort(arr);
-		printArray(arr);
-	}
+        int[] arr = generateRandomArray(maxSize, maxValue);
+        printArray(arr);
+        bubbleSort(arr);
+        printArray(arr);
+    }
 
 }
